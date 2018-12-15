f80e1d629c13330ce87fd76bd059041636f01b49
denshv <rbbozk@yandex.ru>
2018-12-15 15:44:23 +0300

    problem 1

---
+++ Task3/problem1.cpp
@@ -0,0 +1,35 @@
+#include <atomic>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+int main() {
+    size_t count = 0;
+    std::atomic<bool> done(false);
+    std::mutex mut;
+    std::queue<int> items;
+
+    std::thread producer([&]() {
+        for (int i = 0; i < 10000; ++i) {
+            std::lock_guard<std::mutex> guard(mut);
+            items.push(i);
+            count++;
+        }
+        done = true;
+    });
+
+    std::thread consumer([&]() {
+        while (!done) {
+            std::lock_guard<std::mutex> guard(mut);
+            while (!items.empty()) {
+                items.pop();
+                count--;
+            }
+        }
+    });
+
+    producer.join();
+    consumer.join();
+    std::cout << count << std::endl;
+}
